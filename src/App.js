5537b329eb0e250cb3058b9c09c718df509a9a2e
KailerS <KailerS@users.noreply.github.com>
2019-11-21 14:17:58 -0700

    Added the correct routing to the pages needed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <>
-      <h1>Coming Soon</h1>
-    </>      
+    <Router>
+      <>
+        <Switch>
+          <Route exact path={"/"} component={Home}/>
+          <Route exact path={"/about"} component={About}/>
+          <Route exact path={"/projects"} component={Projects}/>
+          <Route exact path={"/contact"} component={Contact}/>
+        </Switch>
+      </>      
+    </Router>
   );
 }
 
---
+++ src/pages/Projects.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Project () {
+    return (
+        <h1>This is the Projects page</h1>
+    )
+}
+
+
+export default Project;
