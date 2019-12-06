78b3a4e1d1d024360c5fb2d632c7781b559f9b72
KailerS <KailerS@users.noreply.github.com>
2019-12-06 15:22:05 -0700

    changed home page path

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <>
         <Switch>
-          <Route exact path={"/"} component={Home}/>
+          <Route exact path={"/portfolio"} component={Home}/>
           <Route exact path={"/about"} component={About}/>
           <Route exact path={"/projects"} component={Projects}/>
           <Route exact path={"/contact"} component={Contact}/>
