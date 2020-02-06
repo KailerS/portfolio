274c3eab6f5f3dbfd91cb4a0f13c527760cdfa3a
KailerS <KailerS@users.noreply.github.com>
2020-02-05 20:51:40 -0700

    Added some comments for clarity

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+// Hash route to make sure it deploys properly to gitpages
 import { HashRouter, Route, Switch } from "react-router-dom";
+// Imports of all my  pages and the footer component
 import Home from "./pages/Home/index";
 import About from "./pages/About/index";
 import Projects from "./pages/Projects";
@@ -11,10 +13,12 @@ function App() {
     <HashRouter basename="/">
       <>
         <Switch>
+          {/* Switches pages based on what path the user hits to act like a multi page website */}
           <Route exact path={"/"} component={Home}/>
           <Route exact path={"/about"} component={About}/>
           <Route exact path={"/projects"} component={Projects}/>
           <Route exact path={"/contact"} component={Contact}/>
+          {/* Add a non specific path that displays a 404 if user types in a bad pathway */}
         </Switch>
         <Footer/>
       </>      
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
+// Render what is sent from app to the page
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
