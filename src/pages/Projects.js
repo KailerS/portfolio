19be1bcfbe6ebb3eb8834b9bd87c7de43480ef17
KailerS <KailerS@users.noreply.github.com>
2020-02-05 21:11:46 -0700

    Added comments and es6 updtes to the project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,17 +1,23 @@
 import React from "react";
+// Importing the images component into the projects page
 import ImageDiv from "../components/ImageDiv/index";
+// Importing the JSON data featuring my projects into the page
 import data from "../data.json";
+// A nav bar component
 import NavBar from "../components/NavBar";
 
-function Project () {
+const Project =  () => {
     return (
         <>
             <NavBar />
             <div className="container">
+                {/* In-line styling for the div */}
                 <div style={{width:"100%", textAlign:"center"}}>
                     <h1 id="projectHeader">Some Examples of My Work!</h1>
                 </div>
+                {/* Since the projects are in an array it can be mapped through and a div made for each one */}
                 {data.map(project => (
+                    // Creates a div for each project and passes on the following info to the ImageDic component
                     <ImageDiv 
                         key={project.id}
                         id={project.id}
