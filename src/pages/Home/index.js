f26c7a8334dbea780b5e50777a4004779470aa3c
KailerS <KailerS@users.noreply.github.com>
2020-02-05 21:03:31 -0700

    added comments and some es6 format

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,21 @@
 import React from "react";
+// Takes the style sheet for the homepage
 import "./styles.css";
+// Makes it so links work properly inside a single page application
 import { Link } from "react-router-dom";
 
-function Home () {
+const Home = () => {
     return (
         <>
             <div id="homepageDiv">
                 <h1>Kailer Smith</h1>
                 <h2>Web Developer</h2>
+                {/* Link to get to the about page */}
                 <h2><Link to="/about">Get to Know More!</Link></h2>
                 <h3>Click the icons in the footer to find my github profile and my LinkedIn page!</h3>  
             </div>
             <div id="homeIconHolder">
+                {/* Images of languages */}
                 <img src="./assets/images/react.png" alt="react"/>
                 <img src="./assets/images/html.webp" alt="html"/>
                 <img src="./assets/images/css.webp" alt="css"/>
