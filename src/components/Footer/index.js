44e3bbcfc9d709201b98011b8e10fbbaa123185a
KailerS <KailerS@users.noreply.github.com>
2020-02-07 15:59:58 -0700

    Added comments and es6 format

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,23 @@
 import React from "react";
+// Importing my css file
 import "./style.css";
+// Makes sure the Links work properly
 import { Link } from "react-router-dom";
 
-function Footer () {
+// Footer component
+const Footer = () => {
     return (
         <footer>
                 <div id="footerDiv">
+                    {/* Link to github with a github icon */}
                     <a href="https://github.com/KailerS" target="blank">
                         <i className="fab fa-github-square fa-3x"></i>
                     </a>
+                    {/* Link to the home page with a house icon */}
                     <Link to="/">
                         <i className="fas fa-home fa-3x"></i>
                     </Link>
+                    {/* Link to LinkedIn with a LinkedIn icon */}
                     <a href="https://www.linkedin.com/in/kailer-smith-4a5a9711b/" target="blank">
                         <i className="fab fa-linkedin fa-3x"></i>
                     </a>
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,21 +1,26 @@
 import React from "react";
+// Imports my css file
 import "./style.css";
+// Imports the ability to link properly on a single page app
 import { Link } from "react-router-dom";
 
 
-function NavBar () {
+const NavBar = () => {
     return (
         <nav>
             <div className="navHolder">
                 <h1>Kailer Smith</h1>
                 <ul>
                     <li>
+                        {/* Links to about me page */}
                         <Link to="/about">About</Link> 
                     </li>
                     <li>
+                        {/* Links to my projects */}
                         <Link  to="/projects">Projects</Link>
                     </li>
                     <li>
+                        {/* Link to my contact */}
                         <Link to ="/contact">Contact</Link>
                     </li>
                 </ul>
